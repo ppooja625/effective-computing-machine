26431c489320401ef2c01c68a905de9c54c558e8
ppooja625 <ppooja625@gmail.com>
2023-03-31 07:54:39 +0530

    added gitignore

--- scrape-github.py
+++ scrape-github.py
@@ -2,65 +2,66 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-base_url = 'https://github.com'
 
-def scrape_topic_repositories(topic, path=None):
-    """Get the top repositories for a topic and write them to a CSV file"""
-    if path is None:
-        path = topic + '.csv'
-    topic_page_doc = get_topic_page(topic)
-    topic_repositories = get_top_repositories(topic_page_doc)
-    write_csv(topic_repositories, path)
-    print('Top repositories for topic "{}" written to file "{}"'.format(topic, path))
-    return path
 
+base_url='https://github.com/topics/'
 def get_top_repositories(doc):
-    """Parse the top repositories for a topic given a Beautiful Soup document"""
-    article_tags = doc.find_all('article', class_='border rounded color-shadow-small color-bg-subtle my-4')
-    topic_repos = [parse_repository(tag) for tag in article_tags]
-    return topic_repos
+    article_tags=doc.find_all('article',class_='border rounded color-shadow-small color-bg-subtle my-4')
+    top_repos=[parse_repository(tag) for tag in article_tags]
+    return top_repos
 
 def get_topic_page(topic):
-    """Get the web page containing the top repositories for a topic as a Beautiful Soup document"""
-    topic_repos_url = 'https://github.com/topics/' + topic
-    response = requests.get(topic_repos_url)
-    if response.status_code != 200:
-        print('Status code:', response.status_code)
-        raise Exception('Failed to fetch web page ' + topic_repos_url)
+    topic_url=base_url+topic
+    response=requests.get(topic_url)
+    if not response.ok:
+        print('response status:',response.status_code)
+        raise Exception('failed to fetch the webpage'+topic_url)
     doc=BeautifulSoup(response.text)
-    return doc   
-
-def parse_repository(article_tag):
-    """Parse information about a repository from an <article> tag"""
-    a_tags = article_tag.h1.find_all('a')
-    username = a_tags[0].text.strip()
-    repo_name = a_tags[1].text.strip()
-    repo_url = base_url + a_tags[1]['href'].strip()
-    stars_tag = article_tag.find('a', class_='social-count float-none')
-    star_count = parse_star_count(stars_tag.text.strip())
+    return doc
+    
+def parse_repository(article_tags):
+    a_tag=article_tags.h3.find_all('a')
+    username=a_tag[0].text.strip()
+    repo_name=a_tag[1].text.strip()
+    repo_url=base_url+a_tag[1]['href'].strip()
+    star_tag=article_tags.find('span',class_='Counter js-social-count')
+    star_count=parse_star_count(star_tag.text.strip())
     return {'repository_name': repo_name, 'owner_username': username, 'stars': star_count, 'repository_url': repo_url}
 
-def parse_star_count(stars_str):
-    """Parse strings like 40.3k and get the no. of stars as a number"""
-    stars_str = stars_str.strip()
-    return int(float(stars_str[:-1]) * 1000) if stars_str[-1] == 'k' else int(stars_str)
-
-def write_csv(items, path):
-    """Write a list of dictionaries to a CSV file"""
-    with open(path, 'w') as f:
-        if len(items) == 0:
+def parse_star_count(star_str):
+    star_str=star_str.strip()
+    if star_str[-1]=="k":
+        return int(float(star_str[:-1]))*1000
+    else:
+        int(star_str)
+    
+def write_csv(items,path):
+    with open(path,'w') as f:
+        if len(items)==0:
             return
-        headers = list(items[0].keys())
-        f.write(','.join(headers) + '\n')
+        headers=list(items[0].keys())
+        f.write(','.join(headers)+'\n')
+
         for item in items:
-            values = []
+            values=[]
             for header in headers:
-                values.append(str(item.get(header, "")))
-            f.write(','.join(values) + "\n")
+                values.append(str(item.get(header,' ')))
+            f.write(','.join(values)+'\n')
+                
+def scrap_topic_reperositories(topic,path=None):
+    if path is None:
+        path=topic+'.csv'
+        
+    topic_page_doc=get_topic_page(topic)
+    topic_repositories=get_top_repositories(topic_page_doc)
+    write_csv(topic_repositories,path)
+    print('Top repositiories for {} written to path {} is'.format(topic,path))
+    return path
+        
 
             
 
 if __name__=="__main__":
     #print(sys.argv[1])
     #topic=sys.argv[1]
-    scrape_topic_repositories('machine-learning')
+   scrap_topic_reperositories('Azure')
